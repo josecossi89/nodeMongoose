e2d2a5631b519ff69d7d01fee6e86ac332f99642
Jose Cossi <josecossib@gmail.com>
2022-04-01 10:25:55 -0300

    controller tracks added and corrected

---
+++ controllers/tracks.js
@@ -0,0 +1,36 @@
+/**
+ * Obtener todos los registros de la base de datos
+ * @param {*} req
+ * @param {*} res
+ */
+const getItems = (req, res) => {};
+
+/**
+ * Obtener un detalle en particular de algun registro en la base de datos
+ * @param {*} req
+ * @param {*} res
+ */
+const getItem = (req, res) => {};
+
+/**
+ * Crear un nuevo registro en la base de datos
+ * @param {*} req
+ * @param {*} res
+ */
+const createItems = (req, res) => {};
+
+/**
+ * Actualizar un registro en la base de datos
+ * @param {*} req
+ * @param {*} res
+ */
+const updateItems = (req, res) => {};
+
+/**
+ * Eliminar un registro de la base de datos
+ * @param {*} req
+ * @param {*} res
+ */
+const deleteItems = (req, res) => {};
+
+module.exports = { getItems, getItem, createItems, deleteItems, updateItems };

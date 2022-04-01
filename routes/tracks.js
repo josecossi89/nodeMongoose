3e82dc9ee6bbfa8ead4477042dd432333d12ec81
Jose Cossi <josecossib@gmail.com>
2022-04-01 10:34:28 -0300

    route get in file tracks modified

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,15 +1,18 @@
 const express = require("express");
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItems,
+  deleteItems,
+} = require("../controllers/tracks");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  try {
-    const data = ["Hola", "Mundo"];
-    res.send({ data });
-    console.log(`Hola Soy un track`);
-  } catch (error) {
-    res.status(500).json({ message: `Error in line` });
-  }
-});
+router.get("/", getItems);
+router.get("/:id", getItem);
+router.post("/", createItem);
+router.put("/:id", updateItems);
+router.delete("/:id", deleteItems);
 
 module.exports = router;

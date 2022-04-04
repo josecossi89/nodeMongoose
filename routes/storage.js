67aa36c68d4f01aac89903d44d6c9a1f0f062cdd
Jose Cossi <josecossib@gmail.com>
2022-04-03 23:04:10 -0300

    modified route post

--- routes/storage.js
+++ routes/storage.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItems,
+  deleteItems,
+} = require("../controllers/storage.js");
 
 const router = express.Router();
 const uploadMiddleware = require("../utils/handleStorage.js");
 
-router.post("/", uploadMiddleware.single("myfile"), (req, res) => {
-  res.send({ a: 1 });
-});
+router.post("/", uploadMiddleware.single("myfile"), createItem);
 
 module.exports = router;

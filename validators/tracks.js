276ebf3d25305a156d6864379114d6ec9b02f21d
Jose Cossi <josecossib@gmail.com>
2022-04-05 09:48:58 -0300

    corrections in validators tracks file

--- validators/tracks.js
+++ validators/tracks.js
@@ -4,12 +4,14 @@ const validatorCreateItem = [
   check("name").exists().notEmpty(),
   check("album").exists().notEmpty(),
   check("cover").exists().notEmpty(),
+  check("artist").exists().notEmpty(),
   check("artist.name").exists().notEmpty(),
   check("artist.nickname").exists().notEmpty(),
   check("artist.nationality").exists().notEmpty(),
-  check("duration.start").exists().notEmpty().isLength({ min: 0, max: 1 }),
-  check("duration.end").exists().notEmpty().isLength({ min: 0, max: 1 }),
-  check("mediaId").exists().notEmpty(),
+  check("duration").exists().notEmpty(),
+  check("duration.start").exists().notEmpty(),
+  check("duration.end").exists().notEmpty(),
+  check("mediaId").exists().notEmpty().isMongoId(),
 ];
 
 module.exports = validatorCreateItem;

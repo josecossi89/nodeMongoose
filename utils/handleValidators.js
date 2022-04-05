a28601656e5a28c807b231e3d882b530a8eec9d1
Jose Cossi <josecossib@gmail.com>
2022-04-05 11:07:51 -0300

    validators corrections

--- utils/handleValidators.js
+++ utils/handleValidators.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator");
 const validateResults = (req, res, next) => {
   try {
     validationResult(req).throw();
-    return next;
+    return next();
   } catch (err) {
     res.status(403);
     res.send({ error: err.array() });

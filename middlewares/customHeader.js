a070025ebfbe78afd7af18c37918f9552abd86f6
Jose Cossi <josecossib@gmail.com>
2022-04-06 09:40:25 -0300

    middleware customHeader created and exports

--- middlewares/customHeader.js
+++ middlewares/customHeader.js
@@ -1,3 +1,16 @@
-const customHeader = () => {};
+const customHeader = (req, res, next) => {
+  try {
+    const apiKey = req.headers.api_key;
+    if (apiKey === "josecossi-01") {
+      next();
+    } else {
+      res.status(403);
+      res.send({ error: `ERROR_EN_API_KEY` });
+    }
+  } catch (error) {
+    res.status(403);
+    res.send({ error: `ALGO_OCURRIO_EN_EL_CUSTOM_HEADER` });
+  }
+};
 
 module.exports = customHeader;
